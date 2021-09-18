94239ec879e940495377f729eb19bd8c367eb87b
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-18 17:21:24 +0100

    Added tests and solution for both length and average

--- array-analysis.js
+++ array-analysis.js
@@ -1,7 +1,15 @@
 const analyze = function analyze(array) {
   return {
     length: array.length,
+    average: getAverage(array),
   };
 };
 
+function getAverage(array) {
+  const sum = array.reduce((prev, cur) => {
+    return prev + cur;
+  }, 0);
+
+  return sum / array.length;
+}
 export { analyze };
--- array-analysis.test.js
+++ array-analysis.test.js
@@ -1,11 +1,6 @@
 import { analyze } from './array-analysis';
 
-test('Test length', () => {
+test('Test Length and Average', () => {
   const object = analyze([1, 8, 3, 4, 2, 6]);
-  expect(object).toEqual({ length: 6 });
-});
-
-test('Test length', () => {
-  const object = analyze([1, 8, 3, 4, 2]);
-  expect(object).toEqual({ length: 5 });
+  expect(object).toEqual({ length: 6, average: 4 });
 });
