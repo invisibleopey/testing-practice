9cc41c9dfd5b58c10d7afd221b143106df044482
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-18 04:16:04 +0100

    Passed test with multiple words string

--- index.test.js
+++ index.test.js
@@ -6,3 +6,7 @@ test('Capitalizes first letter of string', () => {
 test('Capitalizes first letter of string', () => {
   expect(capitalize('abdullahi')).toBe('Abdullahi');
 });
+
+test('Capitalizes first letter of string', () => {
+  expect(capitalize('the boy is back.')).toBe('The boy is back.');
+});
