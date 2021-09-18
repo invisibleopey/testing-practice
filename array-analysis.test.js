9137d091936bc5daaadbeff16fc50747068175c7
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-18 17:36:17 +0100

    Added individual tests for all the properties of object returned

--- array-analysis.test.js
+++ array-analysis.test.js
@@ -1,5 +1,25 @@
 import { analyze } from './array-analysis';
 
+test('Test value of length', () => {
+  const object = analyze([1, 8, 3, 4, 2, 6]);
+  expect(object.length).toBe(6);
+});
+
+test('Test value of min', () => {
+  const object = analyze([1, 8, 3, 4, 2, 6]);
+  expect(object.min).toBe(1);
+});
+
+test('Test value of max', () => {
+  const object = analyze([1, 8, 3, 4, 2, 6]);
+  expect(object.max).toBe(8);
+});
+
+test('Test value of Average', () => {
+  const object = analyze([1, 8, 3, 4, 2, 6]);
+  expect(object.average).toBe(4);
+});
+
 test('Test Length, min, max and Average', () => {
   const object = analyze([1, 8, 3, 4, 2, 6]);
   expect(object).toEqual({ length: 6, average: 4, min: 1, max: 8 });
