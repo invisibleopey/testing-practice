9accd159cfa65128d5c8dbbabc28c4fe74020740
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-18 15:56:10 +0100

    Solved the first two tests

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -1,7 +1,14 @@
-const caesarCipher = {
-  encrypt(string, key) {
-    return 'bcdef';
-  },
+const caesarCipher = function caesarCipher(string, key) {
+  const alpahbets = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const lowerCaseString = string.toLowerCase();
+  let newString = '';
+  for (let i = 0; i < lowerCaseString.length; i += 1) {
+    let currentLetter = lowerCaseString[i];
+    let currentIndex = alpahbets.indexOf(currentLetter);
+    let newIndex = currentIndex + key;
+    newString += alpahbets[newIndex];
+  }
+  return newString;
 };
 
 export { caesarCipher };
--- caesar-cipher.test.js
+++ caesar-cipher.test.js
@@ -1,5 +1,9 @@
 import { caesarCipher } from './caesar-cipher';
 
 test('5 letters with key 1', () => {
-  expect(caesarCipher.encrypt('abcde', 1)).toBe('bcdef');
+  expect(caesarCipher('abcde', 1)).toBe('bcdef');
+});
+
+test('4 letters with key 1', () => {
+  expect(caesarCipher('abcd', 1)).toBe('bcde');
 });
