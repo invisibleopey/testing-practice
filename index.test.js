daa41f53ef762a8cf7d4ebbd93bf5d09bdd714e3
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-18 04:14:41 +0100

    Passed tests of capitalize with triangulation. Using a single word

---
+++ index.test.js
@@ -0,0 +1,8 @@
+import capitalize from './index';
+test('Capitalizes first letter of string', () => {
+  expect(capitalize('opeyemi')).toBe('Opeyemi');
+});
+
+test('Capitalizes first letter of string', () => {
+  expect(capitalize('abdullahi')).toBe('Abdullahi');
+});
