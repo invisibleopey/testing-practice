2a82eacf975ef8fb01514ee60c928d93ce864951
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-09-18 04:41:12 +0100

    Passed reverseString test using triangulation

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ const capitalize = function capitalize(string) {
   stringArray[0] = stringArray[0].toUpperCase();
   return stringArray.join('');
 };
-export default capitalize;
+const reverseString = function reverseString(string) {
+  const stringArray = string.split('');
+  const reversedArray = [];
+  for (let i = stringArray.length; i >= 0; i -= 1) {
+    reversedArray.push(stringArray[i]);
+  }
+  return reversedArray.join('');
+};
+export { capitalize, reverseString };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,6 @@
-import capitalize from './index';
+import { capitalize, reverseString } from './index';
+
+// Capitalize(string)
 test('Capitalizes first letter of string', () => {
   expect(capitalize('opeyemi')).toBe('Opeyemi');
 });
@@ -9,4 +11,13 @@ test('Capitalizes first letter of string', () => {
 
 test('Capitalizes first letter of string', () => {
   expect(capitalize('the boy is back.')).toBe('The boy is back.');
+});
+
+// reverseString(string)
+test('Reverse strings', () => {
+  expect(reverseString('boy')).toBe('yob');
+});
+
+test('Reverse strings', () => {
+  expect(reverseString('girl')).toBe('lrig');
 });
